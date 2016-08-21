c58b222594b91ca552ee0d1585def36df8000660
Tim Pietz <t.pietz97@gmail.com>
2016-08-21 20:41:49 +0200

    Fix FPS target

--- src/GameLoopThread.java
+++ src/GameLoopThread.java
@@ -7,42 +7,54 @@ import javax.sql.rowset.spi.SyncFactory;
  * Created by tim on 8/21/16.
  */
 public class GameLoopThread extends Thread {
-    boolean isRunning;
-    GameField gameField;
 
-     public GameLoopThread(GameField gameField) {
-         this.gameField = gameField;
-         isRunning = true;
-     }
+    private static final long MILLISECOND = 1_000_000;
+    private static final long TARGET_FRAMETIME = 1_000_000_000 / Constants.FPS;
+
+    private GameField gameField;
+    private boolean isRunning;
+
+    public GameLoopThread(GameField gameField) {
+        this.gameField = gameField;
+        isRunning = true;
+    }
+
 
     /**
      * TODO static fps -> dynamic fps (do not use sleep)
      */
     @Override
-     public void run() {
-         long TPS = 1000 / Constants.FPS;
-         long startTime, sleepTime;
+    public void run() {
+        long startTime, endTime, sleepTime;
+        long sleepTimeMs, sleepTimeNs;
 
-         while (isRunning) {
 
-             startTime = System.nanoTime();
+        while (isRunning) {
 
-             //synchronized (gameField) {
-                 gameField.repaint();
-                //System.out.println(System.nanoTime()-startTime);
-             //}
+            startTime = System.nanoTime();
 
-             sleepTime = TPS - System.nanoTime() - startTime;
-             try {
-                 if (sleepTime > 0) {
-                     sleep(sleepTime);
-                 }
-             } catch (Exception ignored) { }
-         }
-     }
+            //synchronized (gameField) {
+            gameField.repaint();
+            //System.out.println(System.nanoTime()-startTime);
+            //}
 
-     public void setRunning(boolean running) {
-         isRunning = running;
-     }
+            endTime = System.nanoTime();
+
+            sleepTime = TARGET_FRAMETIME - endTime + startTime;
+            sleepTimeMs = sleepTime / MILLISECOND;
+            sleepTimeNs = sleepTime % MILLISECOND;
+
+            try {
+                if (sleepTime > 0) {
+                    sleep(sleepTimeMs, (int) sleepTimeNs);
+                }
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
 
 }
