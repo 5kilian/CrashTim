e807549be9c37f97de0d904a49df6037252a2edd
Tim Pietz <t.pietz97@gmail.com>
2016-09-04 16:33:46 +0200

    Correct ModelConstants

--- src/constants/ModelConstants.java
+++ src/constants/ModelConstants.java
@@ -10,6 +10,6 @@ public class ModelConstants {
     public static final Color WIREFRAME_COLOR = Color.DARK_GRAY;
 
     public static final String COIN_SOURCE = "resources/objects/coin.cto";
-    public static final String CAR_SOURCE = "resources/objects/car.cto";
+    public static final String CAR_SOURCE = "resources/objects/movable/car.cto";
 
 }
