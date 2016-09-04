e823b370116d3b73ddc9f8854d625632a4924f78
Tim Pietz <t.pietz97@gmail.com>
2016-09-04 17:38:28 +0200

    Correct model folder structure and constants

--- src/constants/ModelConstants.java
+++ src/constants/ModelConstants.java
@@ -9,7 +9,7 @@ public class ModelConstants {
 
     public static final Color WIREFRAME_COLOR = Color.DARK_GRAY;
 
-    public static final String COIN_SOURCE = "resources/objects/coin.cto";
+    public static final String COIN_SOURCE = "resources/objects/immovable/coin.cto";
     public static final String CAR_SOURCE = "resources/objects/movable/car.cto";
 
 }
